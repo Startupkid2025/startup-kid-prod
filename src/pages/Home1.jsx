7c275e4de72a878b27cb29de77fec828532b2b6f
Omer Gabay <omer@startupkid.co.il>
2025-12-17 12:24:05 +0000

    File changes

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -470,9 +470,9 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 space-y-8">
           <Avatar 
-            userData={userData} 
-            onOpenWardrobe={() => setShowWardrobe(true)}
-            onOpenWork={() => setShowWork(true)}
+            stage={1}
+            totalLessons={userData?.total_lessons || 0}
+            equippedItems={userData?.equipped_items || {}}
           />
 
           <motion.div
