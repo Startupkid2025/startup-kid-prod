9fa2fe58b8d7e0d31425225193a836d8f993c9fd
Omer Gabay <omer@startupkid.co.il>
2025-12-18 15:28:47 +0000

    File changes

--- src/pages/MathGames1.jsx
+++ src/pages/MathGames1.jsx
@@ -583,8 +583,7 @@ ${question} = ${correctAnswer}
           const leaderboardEntries = await base44.entities.LeaderboardEntry.filter({ student_email: userData.email });
           if (leaderboardEntries.length > 0) {
             await base44.entities.LeaderboardEntry.update(leaderboardEntries[0].id, {
-              coins: newCoins,
-              total_math_earnings: (userData.total_math_earnings || 0) + coinsEarned
+              coins: newCoins
             });
           }
         } catch (leaderboardError) {
